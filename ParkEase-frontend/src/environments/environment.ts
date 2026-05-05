9947d6b75b318d71b4ffac423df5a634633b3d54
HaR-S-H <h8551283@gmail.com>
2026-05-05 15:32:04 +0530

    refactor: remove unused components and styles from manager and driver dashboards
    - Deleted manager-dashboard-page component and its associated styles.
    - Removed my-bookings-page component and its styles.
    - Deleted my-vehicles-page component and its styles.
    - Cleaned up environment configuration and index files.
    - Removed unused favicon and global styles.
    - Deleted TypeScript configuration files related to the Angular application.

--- ParkEase-frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiGatewayBaseUrl: 'http://localhost:5242/api/v1',
-  googleClientId: '862280762117-morkaunor8lf2d1o1q0pajp6ubhhsu4n.apps.googleusercontent.com',
-};
